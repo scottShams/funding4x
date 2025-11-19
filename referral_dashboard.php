bd0a5b9a6e3303853be0d1cdc0e2cb74e53b83ce
scottShams <scott123.staff.india@gmail.com>
2025-11-19 16:46:43 +0600

    Add Tawk.to live chat widget to referral dashboard

--- referral_dashboard.php
+++ referral_dashboard.php
@@ -863,6 +863,19 @@ $progressPercentage = min(($credits / $goalCredits) * 100, 100);
 
     </script>
 
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/691d6280a5da4b195b532b2a/1jadchhls';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+        })();
+    </script>
+
 </body>
 
 </html>
