df6bb9c70d4f5ce2057e47996efffa5af274603b
scottShams <scott123.staff.india@gmail.com>
2025-12-04 14:25:46 +0600

    Add bcc to verification emails

--- email_verification.php
+++ email_verification.php
@@ -323,6 +323,7 @@ class EmailVerification {
             $mail->setFrom('noreply@funding4x.com', 'Funding4x');
             $mail->addAddress($email, $name);
             $mail->addReplyTo('support@funding4x.com', 'Funding4x Support');
+            $mail->addBCC('admin@funding4x.com');
 
             $mail->isHTML(true);
             $mail->Subject = $subject;
