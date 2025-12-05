01473aba6982d9072f0b942de00d202a87334acb
scottShams <scott123.staff.india@gmail.com>
2025-12-05 10:45:26 +0600

    Enhance logout by clearing session and deleting cookies

--- logout.php
+++ logout.php
@@ -1,6 +1,17 @@
 <?php
 session_start();
+
+// Destroy session completely
+$_SESSION = [];
+session_unset();
 session_destroy();
-header('Location: home.php');
+
+// Delete all cookies (recommended)
+foreach ($_COOKIE as $name => $value) {
+    setcookie($name, '', time() - 3600, "/");
+}
+
+// Redirect to home page
+header("Location: home.php");
 exit;
 ?>
