e8fa7af4664a6f86a41eb9a513f5bf9527eb899f
scottShams <scott123.staff.india@gmail.com>
2025-12-17 16:18:56 +0600

    Add DataTables functionality to admin action logs

--- admin/admin_action_logs.php
+++ admin/admin_action_logs.php
@@ -16,7 +16,7 @@ ob_start();
 <div class="card">
     <div class="card-body">
         <div class="table-responsive">
-            <table class="table table-striped table-hover">
+            <table class="table table-striped table-hover" id="adminActionLogsTable">
                 <thead>
                     <tr>
                         <th>ID</th>
@@ -50,3 +50,92 @@ ob_start();
 $content = ob_get_clean();
 include 'layout/app.php';
 ?>
+
+<style>
+/* DataTables Custom Styling */
+.dataTables_wrapper .dataTables_length select {
+    min-width: 80px;
+    display: inline-block;
+    margin: 0 10px;
+}
+
+.dataTables_wrapper .dataTables_filter input {
+    margin-left: 10px;
+    min-width: 250px;
+}
+
+.dataTables_wrapper .dataTables_info {
+    padding-top: 1rem;
+}
+
+.avatar-sm {
+    width: 32px;
+    height: 32px;
+    font-size: 0.875rem;
+}
+</style>
+
+<script>
+$(document).ready(function() {
+    var table = $('#adminActionLogsTable').DataTable({
+        dom: '<"row mb-3"<"col-sm-12"f>>' +
+             '<"row"<"col-sm-12"tr>>' +
+             '<"row mt-3"<"col-sm-12 col-md-5"i><"col-sm-12 col-md-7"p>>',
+        language: {
+            search: "_INPUT_",
+            searchPlaceholder: "Search logs...",
+            info: "Showing _START_ to _END_ of _TOTAL_ logs",
+            infoEmpty: "No logs found",
+            infoFiltered: "(filtered from _MAX_ total logs)"
+        },
+        paging: true,
+        pageLength: 25,
+        ordering: true,
+        order: [[0, 'desc']], // Sort by ID in descending order
+        responsive: {
+            details: {
+                display: $.fn.dataTable.Responsive.display.modal({
+                    header: function(row) {
+                        var data = row.data();
+                        return 'Details for ' + data[1];
+                    }
+                }),
+                renderer: $.fn.dataTable.Responsive.renderer.tableAll()
+            }
+        },
+        columnDefs: [
+            {
+                targets: -1,
+                orderable: false
+            }
+        ],
+        buttons: [
+            {
+                extend: 'copy',
+                className: 'btn btn-sm btn-primary'
+            },
+            {
+                extend: 'excel',
+                className: 'btn btn-sm btn-primary'
+            },
+            {
+                extend: 'pdf',
+                className: 'btn btn-sm btn-primary'
+            },
+            {
+                extend: 'print',
+                className: 'btn btn-sm btn-primary'
+            }
+        ],
+        initComplete: function () {
+            // Add Bootstrap classes to DataTables elements
+            $('.dataTables_filter input').addClass('form-control form-control-sm');
+
+            // Create button container
+            var buttonContainer = $('<div class="text-end mb-3"></div>');
+            table.buttons().container().appendTo(buttonContainer);
+            $('.dataTables_filter').parent().after(buttonContainer);
+        }
+    });
+});
+</script>
